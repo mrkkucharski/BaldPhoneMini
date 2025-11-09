277b636c8eccc2a432ca5c8ae43dfb41fc155ef1
Damian Kuzmiak <44574412+DamianKuzmiak@users.noreply.github.com>
2025-11-09 11:02:35 +0100

    build: Enable Kotlin to the project
    * Bump Gradle version (8.10 → 8.11) and AGP (8.8 → 8.9)
    * Update dependencies
    * Fix: Configure Glide annotation processor
    * Untrack .github directory

---
+++ app/src/main/java/app/baldphone/neo/MyAppGlideModule.kt
@@ -0,0 +1,10 @@
+package app.baldphone.neo
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * This class is required for Glide's annotation processor.
+ */
+@GlideModule
+class MyAppGlideModule : AppGlideModule()
